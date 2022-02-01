1f680599aa79650e52c7fce7f164be0f564d659c
KNarendra99599 <82750843+KNarendra99599@users.noreply.github.com>
2022-02-01 07:22:35 +0530

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "subnet1-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    public_subnet1_cidr = "10.1.1.0/24"
+
+    tags = {
+        Name = "${var.public_subnet1_name}"
+    }
+}
