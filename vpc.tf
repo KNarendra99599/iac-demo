addc627885a3bf296608abd540b0c18d7f4c5b7d
KNarendra99599 <82750843+KNarendra99599@users.noreply.github.com>
2022-02-01 07:10:47 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,14 @@
 resource "aws_vpc" "main" {
   cidr_block       = "10.1.0.0/16"
-  public_subnet1_cidr = "10.1.1.0/24"
+  
   instance_tenancy = "default"
-  public_subnet1_name = "Terraform_Public_Subnet1-testing"
-
+  
   tags = {
     Name = "demo-vpc"
     Purpose = "Jenkins"
   }
+  public_subnet1_cidr = "10.1.1.0/24"
+  public_subnet1_name = "Terraform_Public_Subnet1-testing"
 }
 
  
