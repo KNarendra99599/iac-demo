48a0c15ac7db731f051fa2c4261e595023446960
KNarendra99599 <82750843+KNarendra99599@users.noreply.github.com>
2022-02-01 07:22:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,14 +8,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnet1-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    public_subnet1_cidr = "10.1.1.0/24"
 
-    tags = {
-        Name = "${var.public_subnet1_name}"
-    }
-}
 
 
  
