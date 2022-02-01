7298f16c69ce2095a3a28a30081080ffae5209da
KNarendra99599 <82750843+KNarendra99599@users.noreply.github.com>
2022-02-01 07:06:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,11 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.1.0.0/16"
+  public_subnet1_cidr = "10.1.1.0/24"
   instance_tenancy = "default"
 
   tags = {
     Name = "demo-vpc"
     Purpose = "Jenkins"
-  }
-  
-    cidr_block       = "172.31.0.0/16"
-  instance_tenancy = "default 1"
-
-  tags = {
-    Name = "Jenkins-demo-vpc"
-    Purpose = "Jenkins."
   }
 }
 
